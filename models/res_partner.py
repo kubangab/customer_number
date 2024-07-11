cf6d62023d2ceb0219d7f13d1a31b324e486e936
kubangab <142019179+kubangab@users.noreply.github.com>
2024-07-11 19:41:40 +0200

    Fixed a couple errors in tests
    With this update we fixes a couple of errors that is caught in the test on Odoo.sh

--- models/res_partner.py
+++ models/res_partner.py
@@ -18,6 +18,18 @@ class ResPartner(models.Model):
          _('The customer number must be unique per company!'))
     ]
     
+    @api.constrains('customer_number', 'company_id')
+    def _check_unique_customer_number(self):
+        for partner in self:
+            if partner.customer_number:
+                domain = [
+                    ('customer_number', '=', partner.customer_number),
+                    ('company_id', '=', partner.company_id.id),
+                    ('id', '!=', partner.id)
+                ]
+                if self.search_count(domain) > 0:
+                    raise exceptions.ValidationError(_('The customer number must be unique per company!'))
+                        
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
@@ -38,22 +50,19 @@ class ResPartner(models.Model):
         digits = int(self.env['ir.config_parameter'].sudo().get_param('customer_number.digits', '5'))
         start = int(self.env['ir.config_parameter'].sudo().get_param('customer_number.start', '1000'))
         padding = self.env['ir.config_parameter'].sudo().get_param('customer_number.padding', 'True').lower() == 'true'
-        
+            
         _logger.info(f"Generating next customer number. Digits: {digits}, Start: {start}, Padding: {padding}")
-        
-        highest_partner = self.search([('customer_number', '!=', False)], order='customer_number desc', limit=1)
-        if highest_partner:
-            try:
-                highest_number = int(highest_partner.customer_number)
-                next_number = max(highest_number + 1, start)
-            except ValueError:
-                _logger.warning(f"Could not convert customer number '{highest_partner.customer_number}' to integer. Using start number.")
-                next_number = start
-        else:
-            next_number = start
+            
+        self.env.cr.execute("""
+            SELECT MAX(CAST(customer_number AS INTEGER))
+            FROM res_partner
+            WHERE customer_number ~ '^[0-9]+$'
+        """)
+        max_number = self.env.cr.fetchone()[0] or start - 1
+        next_number = max(max_number + 1, start)
             
         _logger.info(f"Next number before padding: {next_number}")
-        
+            
         if padding:
             result = str(next_number).zfill(digits)
         else:
