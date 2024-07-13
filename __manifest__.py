303b59a672b703186e1a0e9e305451bc8862917f
kubangab <142019179+kubangab@users.noreply.github.com>
2024-07-13 14:12:11 +0200

    Update __manifest__.py
    Updated version number to 16.0.3.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Customer Number',
-    'version': '16.0.3.0.0',
+    'version': '16.0.3.0.1',
     'category': 'Sales',
     'summary': 'Add Customer Number field to partners',
     'description': """
